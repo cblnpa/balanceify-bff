81607fe7d4a270bd75ad118061a9539a1f152546
cblnpa <camilablnpa@gmail.com>
2025-02-06 11:08:23 -0300

    feat(response): create error and success response class

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,5 @@
 export { AccountModule } from './account/account.module.js';
 export { SubscriptionModule } from './subscription/subscription.module.js';
-export { SummaryModule } from './summary/summary.model.js';
+export { SummaryModule } from './summary/summary.module.js';
 export { TransactionModule } from './transaction/transaction.module.js';
 export { UserModule } from './user/user.module.js';
